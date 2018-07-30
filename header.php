93dc86f0534c6be63956c099f0b60048f3edc7e7
alesna <32298465+alesna97@users.noreply.github.com>
2018-07-30 17:40:43 +0700

    test upload

--- header.php
+++ header.php
@@ -48,25 +48,38 @@
             <div id="content">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container-fluid">
-                        <button type="button" id="sidebarCollapse" class="btn btn-outline-primary btn-rounded">
+                            <div class="col">
+                        <button type="button" id="sidebarCollapse" class="btn btn-outline-light btn-rounded">
                             <i class="fas fa-align-left"></i>
                             <span><img class="icn-35" src="../asset/icons/icons8_Collapse_50px.png"></span>
                        </button>
                        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-align-justify"></i>
                     </button>
-
+                        </div>
+                        <div class="col">
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item"><input type="text" class="form-control" name="search" placeholder="Cari Data" id="search"></li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><img class="icn-35" src="../asset/icons/icons8_Search_50px.png"></a>
+                                <div class="input-group mb-3" style="margin-top: 10px; margin-right: 150px;">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-secondary" type="submit">Cari</button> 
+                                        </div>
+                                    </div>
                             </li>
+                        </ul>
+                    </div>
+                </div>  
+                        <div class="col">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="nav navbar-nav ml-auto">
                             <li class="nav-item">
                                 <a class="nav-link" href="logout.php"><img class="icn-35" src="../asset/icons/icons8_Shutdown_50px.png"></a>
                             </li>
                         </ul>
-                    </div>
+                         </div>
+                        </div>
                     </div>
                 </nav>  
                     <!-- CONTENT BODY -->
--- view/pegawai.php
+++ view/pegawai.php
@@ -1,6 +1,7 @@
 <?php
     require_once "../config/session.php";
     no_session_login();
+    require "../config/conn_db.php";
     include "../header.php";
 ?>
 	<div class="container-fluid">
@@ -23,9 +24,49 @@
             </div>
             <div class="row" style="padding: 15px;">
                  <div class="btn-group">
-                        <button type="button" class="btn btn-secondary">
+                        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#addData">
                             <img src="../asset/icons/icons8_Plus_Math_26px.png">
                         </button>
+                            <!-- ADD DATA -->
+                                    <div class="modal fade" id="addData">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title"> Tambah Data Pegawai </h4>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form action="" name="" method="POST">
+                                                    <div class="form-group">
+                                                        <label for="nama">ID :</label>
+                                                        <input type="text" name="id" class="form-control" id="id">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="nama">Nama :</label>
+                                                        <input type="text" name="nama" class="form-control" id="nama">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="nama">Jabatan :</label>
+                                                        <select name="jabatan" class="custom-select">
+                                                                <option> dummy </option>
+                                                                <option> dummy </option>
+                                                                <option> dummy </option>
+                                                                <option> dummy </option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="nama">Alamat :</label>
+                                                        <input type="text" name="alamat" class="form-control" id="alamat">
+                                                    </div>
+                                                    <button type="button" class="btn btn-rounded btn-primary btn-block" name="btn_tambah_pegawai">Submit</button>
+                                                    <button type="reset" class="btn btn-secondary btn-rounded btn-primary btn-block">Reset</button>
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-block btn-rounded btn-danger" data-dismiss="modal">Batal</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                         <button type="button" class="btn btn-secondary">
                             <img src="../asset/icons/icons8_Waste_26px.png">
                         </button>
@@ -58,11 +99,13 @@
                             <form>
                                 <div class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" id="customCheck" name="checklist">
-                                <label class="custom-control-label" for="customCheck">Checklist</label>
+                                <label class="custom-control-label" for="customCheck">.</label>
                                 </div>
                             </form>
                         </td>
-                        <td><a href="#">Edit</a></td>
+                        <td><a href="#"> Edit </a>
+                            <a href="#"> Detil </a>
+                        </td>
                         <tr>
                         
             		</tbody>

86449ed0da677a13198d293e3bdeaf53ce6651c2
tbfikrif <tbfikrif@gmail.com>
2018-07-31 20:16:04 +0700

    no coment

--- function/hapus_pegawai.php
+++ function/hapus_pegawai.php
@@ -3,13 +3,11 @@ require "../config/conn_db.php";
 
 if (isset($_GET['NIP'])) {
     $NIP = $_GET['NIP'];
-    echo "$NIP";
+    $sql = "DELETE FROM pegawai WHERE NIP='$NIP'";
+    mysqli_query($conn,$sql);
 } else {
     echo "data tidak didapatkan";
 }
-
-$sql = "DELETE FROM pegawai WHERE NIP='$NIP'";
-mysqli_query($conn,$sql);
 
 header('location:../view/pegawai.php');
 ?>
